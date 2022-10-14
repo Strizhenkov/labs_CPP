15153511e2876f2832c242ea728c5ab90b811dc0
Strizhenkov <87713684+Strizhenkov@users.noreply.github.com>
2022-10-14 15:14:41 +0300

    Add files via upload

--- lab2(Libraries + JSON)/main.cpp
+++ lab2(Libraries + JSON)/main.cpp
@@ -1,37 +1,18 @@
 #include <iostream>
-#include <conio.h>
-#include <curl/curl.h>
-#include <nlohmann/json.hpp>
-#include "Request.h"
-#include "Entities.h"
-#include "CurrenciesMath.h"
 
 using namespace std;
 
+template<typename elem, typename... T>
+elem f(elem first, T... array) {
+    return first + f(array...);
+}
+
+template<typename elem>
+elem f(elem value) {
+    return value;
+}
+
 int main() {
-    setlocale(LC_ALL, "ru");
-
-    CurrenciesMath currenciesMath;
-
-    while (!kbhit()) {
-        string jsonString;
-        if(!Request::getUrl("https://www.cbr-xml-daily.ru/daily_json.js", jsonString)) {
-            std::cout << "ERROR curl unable to get url" << std::endl;
-            return 1;
-        }
-
-        nlohmann::json json_data = nlohmann::json::parse(jsonString);
-        DailyQuotes quotes = json_data.get<DailyQuotes>();
-
-        OutputQuotes(quotes);
-        currenciesMath.add(quotes);
-
-        Sleep(1000);
-    }
-
-    cout << "\n\n\n";
-    currenciesMath.Output();
-
-    system("pause");
+    cout << f(1.125, 3, true);
     return 0;
 }
